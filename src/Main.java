8ea46affbfeee7d9902d5386330e689c38c0ceb2
AArvidd <arvid.oscarsson@elev.ga.ntig.se>
2024-05-07 14:34:13 +0200

    code added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.net.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        int portNumber = 5000;
+        if(args.length >= 1){
+            portNumber = Integer.parseInt(args[0]);
+        }
+
+        MulticastSocket serverMulticastSocket = new MulticastSocket(portNumber);
+        System.out.println("MulticastSocket is created at port " + portNumber);
+
+        InetAddress group = InetAddress.getByName("225. 4. 5. 6");
+
+        serverMulticastSocket.joinGroup(group);
+        System.out.println("joinGroup method is called...");
+        boolean infinite = true;
+
+        while(infinite){
+            byte buf[] = new byte[1024];
+            DatagramPacket data = new DatagramPacket(buf, buf.length);
+
+            serverMulticastSocket.receive(data);
+            String msg = new String(data.getData()).trim();
+            System.out.println("message recived from client = " + msg);
+        }
+        serverMulticastSocket.close();
     }
 }
